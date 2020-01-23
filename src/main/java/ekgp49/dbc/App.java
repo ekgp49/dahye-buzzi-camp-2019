6923f5d58821f3b5c8c11b1b05a056c8afe8f3a8
ekgp49 <ekgp4949@gmail.com>
2020-01-23 15:44:12 +0900

    csv파일

--- src/main/java/ekgp49/dbc/App.java
+++ src/main/java/ekgp49/dbc/App.java
@@ -1,5 +1,10 @@
 package ekgp49.dbc;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
@@ -28,14 +33,15 @@ public class App {
   static Deque<String> commandStack = new ArrayDeque<>();
   static Queue<String> commandQueue = new LinkedList<>();
 
+  static List<Information> informationList = new LinkedList<>();
+  static List<Review> reviewList = new LinkedList<>();
+
   public static void main(String[] args) {
+    loadInformationData();
+    loadReviewData();
     Prompt prompt = new Prompt(keyboard);
 
-    List<Information> informationList = new LinkedList<>();
-    List<Review> reviewList = new LinkedList<>();
-
     HashMap<String, Command> commandHandler = new HashMap<>();
-
     commandHandler.put("/search", new SearchCommand(prompt, informationList));
     commandHandler.put("/info/add", new InformationAddCommand(prompt, informationList));
     commandHandler.put("/info/list", new InformationListCommand(informationList));
@@ -75,7 +81,51 @@ public class App {
       } else {
         System.out.println("실행할 수 없는 명령입니다.");
       }
+    }
+    saveInformationData();
+    saveReviewData();
+  }
 
+  private static void loadInformationData() {
+    File file = new File("./information.csv");
+    try (FileReader in = new FileReader(file); Scanner scanData = new Scanner(in);) {
+      while (true) {
+        try {
+          String line = scanData.nextLine();
+          String[] data = line.split(",");
+          Information info = new Information();
+          info.setNo(Integer.parseInt(data[0]));
+          info.setCafeName(data[1]);
+          info.setCafeAddress(data[2]);
+          info.setCafeCall(data[3]);
+          info.setCafeWebSite(data[4]);
+          info.setOpenTime(data[5]);
+          info.setCloseTime(data[6]);
+          info.setHolliday(data[7]);
+          info.setCafeMenu(data[8]);
+          informationList.add(info);
+        } catch (Exception e) {
+          break;
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("파일 읽기 중 오류 발생 - " + e.getMessage());
+    }
+    System.out.printf("총 %d개의 정보 데이터를 로딩했습니다.\n", informationList.size());
+  }
+
+  private static void saveInformationData() {
+    File file = new File("./information.csv");
+    try (FileWriter out = new FileWriter(file);) {
+      for (Information i : informationList) {
+        String line = String.format("%d,%s,%s,%s,%s,%s,%s,%s,%s\n", i.getNo(), i.getCafeName(),
+            i.getCafeAddress(), i.getCafeCall(), i.getCafeWebSite(), i.getOpenTime(),
+            i.getCloseTime(), i.getHolliday(), i.getCafeMenu());
+        out.write(line);
+      }
+      System.out.printf("총 %d개의 정보 데이터를 세이브했습니다.\n", informationList.size());
+    } catch (IOException e) {
+      System.out.println("파일 쓰기 중 오류 발생 - " + e.getMessage());
     }
   }
 
@@ -92,6 +142,48 @@ public class App {
           break;
         }
       }
+    }
+  }
+
+  private static void loadReviewData() {
+    File file = new File("./review.csv");
+    try (FileReader in = new FileReader(file); Scanner scanData = new Scanner(in);) {
+      while (true) {
+        try {
+          String line = scanData.nextLine();
+          String[] data = line.split(",");
+          Review r = new Review();
+          r.setNo(Integer.parseInt(data[0]));
+          r.setCafeName(data[1]);
+          r.setCustomer(data[2]);
+          r.setStarRate(Integer.parseInt(data[3]));
+          r.setContent(data[4]);
+          r.setViewCount(Integer.parseInt(data[5]));
+          r.setCreatedDate(Date.valueOf(data[6]));
+          r.setTimeFormFromToday(data[7]);
+          reviewList.add(r);
+        } catch (Exception e) {
+          break;
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("파일 읽기 중 오류 발생 - " + e.getMessage());
+    }
+    System.out.printf("총 %d개의 리뷰 데이터를 로딩했습니다.\n", reviewList.size());
+  }
+
+  private static void saveReviewData() {
+    File file = new File("./review.csv");
+    try (FileWriter out = new FileWriter(file);) {
+      for (Review r : reviewList) {
+        String line = String.format("%d,%s,%s,%d,%s,%s,%s,%s\n", r.getNo(), r.getCafeName(),
+            r.getCustomer(), r.getStarRate(), r.getContent(), r.getViewCount(), r.getCreatedDate(),
+            r.getTimeFormFromToday());
+        out.write(line);
+      }
+      System.out.printf("총 %d개의 리뷰 데이터를 세이브했습니다.\n", reviewList.size());
+    } catch (IOException e) {
+      System.out.println("파일 쓰기 중 오류 발생 - " + e.getMessage());
     }
   }
 }
--- src/main/java/ekgp49/dbc/domain/Review.java
+++ src/main/java/ekgp49/dbc/domain/Review.java
@@ -3,20 +3,30 @@ package ekgp49.dbc.domain;
 import java.sql.Date;
 
 public class Review {
+  private int no;
+  private String cafeName;
+  private String customer;
+  private int starRate;
+  private String content;
+  private int viewCount;
+  private Date createdDate;
+  private String timeFormFromToday;
+
   @Override
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + ((cafeName == null) ? 0 : cafeName.hashCode());
-    result = prime * result + ((content == null) ? 0 : content.hashCode());
-    result = prime * result + ((createdDate == null) ? 0 : createdDate.hashCode());
-    result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+    result = prime * result + (cafeName == null ? 0 : cafeName.hashCode());
+    result = prime * result + (content == null ? 0 : content.hashCode());
+    result = prime * result + (createdDate == null ? 0 : createdDate.hashCode());
+    result = prime * result + (customer == null ? 0 : customer.hashCode());
     result = prime * result + no;
     result = prime * result + starRate;
-    result = prime * result + ((timeFormFromToday == null) ? 0 : timeFormFromToday.hashCode());
+    result = prime * result + (timeFormFromToday == null ? 0 : timeFormFromToday.hashCode());
     result = prime * result + viewCount;
     return result;
   }
+
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -59,60 +69,67 @@ public class Review {
       return false;
     return true;
   }
-  private int no;
-  private String cafeName;
-  private String customer;
-  private int starRate;
-  private String content;
-  private int viewCount;
-  private Date createdDate; 
-  private String timeFormFromToday;
 
   public int getNo() {
     return no;
   }
+
   public void setNo(int no) {
     this.no = no;
   }
+
   public String getCafeName() {
     return cafeName;
   }
+
   public void setCafeName(String cafeName) {
     this.cafeName = cafeName;
   }
+
   public String getCustomer() {
     return customer;
   }
+
   public void setCustomer(String customer) {
     this.customer = customer;
   }
+
   public int getStarRate() {
     return starRate;
   }
+
   public void setStarRate(int starRate) {
     this.starRate = starRate;
   }
+
   public String getContent() {
     return content;
   }
+
   public void setContent(String content) {
     this.content = content;
   }
+
   public int getViewCount() {
     return viewCount;
   }
+
   public void setViewCount(int viewCount) {
     this.viewCount = viewCount;
   }
+
   public Date getCreatedDate() {
     return createdDate;
   }
+
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
   }
+
   public String getTimeFormFromToday() {
     return timeFormFromToday;
   }
+
   public void setTimeFormFromToday(String timeFormFromToday) {
     this.timeFormFromToday = timeFormFromToday;
   }
